d2286e616e58283ebae91924968e69a332111cf7
Jens Malmlöf <malmlof91@hotmail.com>
2021-11-10 16:58:07 +0100

    continue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Restaurant.create(
+  name: 'Black stone',
+  address: 'gahnsväg',
+  phone_number: '111 333 555'
+)
+  Restaurant.create(
+  name: 'Pirelli',
+  address: 'melonvägen',
+  phone_number: '333 5999 44'
+)
+  Restaurant.create(
+  name: 'Basta',
+  address: 'roadgatan',
+  phone_number: '111222111'
+)
+  Restaurant.create(
+  name: 'Pizzeria Mia',
+  address: 'stengatan',
+  phone_number: '999888000'
+)
+  Restaurant.create(
+  name: 'Fuego',
+  address: 'storgatan',
+  phone_number: '555000777'
+)
